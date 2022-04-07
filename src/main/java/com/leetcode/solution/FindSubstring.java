1773abf52ddb5d00a142b33b6dab43cd2a935218
bowen-wu <13793248162@163.com>
2022-04-07 15:34:43 +0800

    30 FindSubstring move window for hash

--- src/main/java/com/leetcode/solution/FindSubstring.java
+++ src/main/java/com/leetcode/solution/FindSubstring.java
@@ -1,7 +1,13 @@
 package com.leetcode.solution;
 
+/**
+ * https://leetcode-cn.com/problems/substring-with-concatenation-of-all-words/
+ * 30. 串联所有单词的子串
+ */
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class FindSubstring {
@@ -27,6 +33,36 @@ public class FindSubstring {
                 } else {
                     isEqual = false;
                     break;
+                }
+                j += wordLength;
+            }
+            if (isEqual) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    public List<Integer> findSubstringForHash(String s, String[] words) {
+        int wordLength = words[0].length();
+        int windowLength = wordLength * words.length;
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i <= s.length() - windowLength; i++) {
+            String str = s.substring(i, i + windowLength);
+            boolean isEqual = true;
+            HashMap<String, Integer> map = new HashMap<>();
+            for (String word : words) {
+                map.merge(word, 1, Integer::sum);
+            }
+            for (int j = 0; j <= str.length() - wordLength; ) {
+                String word = str.substring(j, j + wordLength);
+                Integer num = map.get(word);
+                if (num == null || num <= 0) {
+                    isEqual = false;
+                    break;
+                } else {
+                    map.put(word, num - 1);
                 }
                 j += wordLength;
             }
