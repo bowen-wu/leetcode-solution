8dfd832a468d314e2fd2e4b2e2ea281f921e371e
bowen-wu <13793248162@163.com>
2022-04-12 15:07:37 +0800

    806 NumberOfLines optimize

--- src/main/java/com/leetcode/solution/NumberOfLines.java
+++ src/main/java/com/leetcode/solution/NumberOfLines.java
@@ -12,30 +12,22 @@ public class NumberOfLines {
     /**
      * 806. 写字符串需要的行数
      * 时间复杂度 O(n)
-     * 空间复杂度
+     * 空间复杂度 O(1)
      */
     public int[] numberOfLines(int[] widths, String s) {
         if (s.length() == 0) {
             return new int[]{0, 0};
         }
-        String words = "abcdefghijklmnopqrstuvwxyz";
 
-        int reminder = 100;
         int lineNumber = 1;
         int lastLineUsed = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            int currentNumber = widths[words.indexOf(currentChar)];
-            if (reminder - currentNumber >= 0) {
-                // 可以减
-                reminder -= currentNumber;
-                lastLineUsed += currentNumber;
-            } else {
-                // 换行
-                lineNumber++;
-                reminder = 100 - currentNumber;
+            int currentNumber = widths[s.charAt(i) - 'a'];
+            lastLineUsed += currentNumber;
+            if (lastLineUsed > 100) {
                 lastLineUsed = currentNumber;
+                lineNumber++;
             }
         }
 
