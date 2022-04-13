3d779a55616a66e7fb2a4562da33d999098658ae
bowen-wu <13793248162@163.com>
2022-04-13 21:03:11 +0800

    380 变长数组 + 哈希桶

--- src/main/java/com/leetcode/solution/RandomizedSet.java
+++ src/main/java/com/leetcode/solution/RandomizedSet.java
@@ -1,52 +1,66 @@
 package com.leetcode.solution;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * https://leetcode-cn.com/problems/insert-delete-getrandom-o1/
  * 380. O(1) 时间插入、删除和获取随机元素
+ * 变长数组 + 哈希桶
+ * HashMap {}
+ * Array []
+ * insert 1
+ * => {1: 0} [1]
+ * insert 4
+ * => {1: 0, 4: 1} [1, 4]
+ * insert 2
+ * => {1: 0, 4: 1, 2: 2} [1, 4, 2]
+ * remove 4
+ * => 1. [1, 2, 2] {1: 0, 2: 2} 1
+ * => 2. [1, 2] {1: 0, 2: 1}
  */
 public class RandomizedSet {
-
-    private final ArrayList<Integer> container = new ArrayList<>();
+    private final List<Integer> numbers;
+    private final Map<Integer, Integer> indexMap;
 
     public RandomizedSet() {
+        this.numbers = new ArrayList<>();
+        this.indexMap = new HashMap<>();
     }
 
     // 当元素 val 不存在时，向集合中插入该项，并返回 true ；否则，返回 false
     // 需要先检查是否存在 val
-    // O(n)
+    // O(1)
     public boolean insert(int val) {
-        System.out.println(container);
-        if (contains(val)) {
+        if (indexMap.get(val) != null) {
             return false;
         }
-
-        // O(1)
-        container.add(val);
+        indexMap.put(val, numbers.size());
+        numbers.add(val);
         return true;
     }
 
     // 当元素 val 存在时，从集合中移除该项，并返回 true ；否则，返回 false
     // 需要先检查是否存在 val
-    // O(n)
+    // O(1)
     public boolean remove(int val) {
-        if (contains(val)) {
-            // O(1)
-            container.remove(new Integer(val));
-            return true;
+        if (indexMap.get(val) == null) {
+            return false;
         }
-        return false;
+        int valIndex = indexMap.get(val);
+        int lastIndex = numbers.size() - 1;
+        int lastVal = numbers.get(lastIndex);
+        numbers.set(valIndex, lastVal);
+        indexMap.put(lastVal, valIndex);
+        numbers.remove(lastIndex);
+        indexMap.remove(val);
+        return true;
     }
 
     // O(1)
     public int getRandom() {
-        int index = (int) (Math.random() * container.size());
-        return container.get(index);
-    }
-
-    private boolean contains(int val) {
-        // indexOf(val) >= 0 => O(n)
-        return container.contains(val);
+        return numbers.get((int) (Math.random() * numbers.size()));
     }
 }
