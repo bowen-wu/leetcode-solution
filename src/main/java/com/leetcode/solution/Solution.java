f492b99642420cd03b794af42dee44836c920ec0
bowen-wu <13793248162@163.com>
2022-04-25 10:11:04 +0800

    398 Pick add Cache

--- src/main/java/com/leetcode/solution/Solution.java
+++ src/main/java/com/leetcode/solution/Solution.java
@@ -1,7 +1,9 @@
 package com.leetcode.solution;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * https://leetcode-cn.com/problems/random-pick-index/
@@ -17,17 +19,24 @@ public class Solution {
     // 时间复杂度：O(n)
     // 空间复杂度：O(1)
     private final int[] nums;
+    private final Map<Integer, List<Integer>> valueIndexCache;
 
     public Solution(int[] nums) {
         this.nums = nums;
+        this.valueIndexCache = new HashMap<>();
     }
 
     public int pick(int target) {
         List<Integer> targetIndexList = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                targetIndexList.add(i);
+        if (valueIndexCache.get(target) != null) {
+            targetIndexList = valueIndexCache.get(target);
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == target) {
+                    targetIndexList.add(i);
+                }
             }
+            valueIndexCache.put(target, targetIndexList);
         }
 
         int randomIndex = (int) (Math.random() * targetIndexList.size());
