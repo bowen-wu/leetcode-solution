056677cbd0192d659f7475ca3a036981cebbf0e6
bowen-wu <13793248162@163.com>
2022-04-26 10:54:47 +0800

    883 ProjectionArea 使用 Max 变量代替数组

--- src/main/java/com/leetcode/solution/ProjectionArea.java
+++ src/main/java/com/leetcode/solution/ProjectionArea.java
@@ -13,25 +13,23 @@ public class ProjectionArea {
     // 空间复杂度：O(1)
     public int projectionArea(int[][] grid) {
         int result = 0;
-        int[] frontViewList = new int[grid.length];
-        int[] sideViewList = new int[grid.length];
         for (int i = 0; i < grid.length; i++) {
+            int sideViewMax = 0;
+            int frontViewMax = 0;
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] > 0) {
                     // 俯视图
                     result += 1;
                 }
-                if (sideViewList[i] < grid[i][j]) {
-                    // 侧视图
-                    result += (grid[i][j] - sideViewList[i]);
-                    sideViewList[i] = grid[i][j];
-                }
-                if (frontViewList[j] < grid[i][j]) {
-                    // 正视图
-                    result += (grid[i][j] - frontViewList[j]);
-                    frontViewList[j] = grid[i][j];
-                }
+
+                // 侧视图
+                sideViewMax = Math.max(sideViewMax, grid[i][j]);
+
+                // 正视图
+                frontViewMax = Math.max(frontViewMax, grid[j][i]);
             }
+            result += sideViewMax;
+            result += frontViewMax;
         }
         return result;
     }
