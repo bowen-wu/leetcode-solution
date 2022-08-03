579eab505cd9dfbaf385ae03c1c6be884a928e98
bowen-wu <13793248162@163.com>
2022-08-03 09:36:23 +0800

    feat: 按奇偶排序数组

---
+++ src/main/java/com/leetcode/solution/array/sortArrayByParity/first/SortArrayByParity.java
@@ -0,0 +1,43 @@
+package com.leetcode.solution.array.sortArrayByParity.first;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.cn/problems/sort-array-by-parity/
+ * 905. 按奇偶排序数组
+ */
+public class SortArrayByParity {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new SortArrayByParity().sortArrayByParity(new int[]{3, 2, 1, 4})));
+    }
+
+    public int[] sortArrayByParity(int[] nums) {
+        // 双指针
+        // leftPoint 第一个奇数
+        // rightPoint 第一个偶数
+        // 时间复杂度：O(n)
+        // 空间复杂度：O(1)
+        int leftPoint = 0;
+        int rightPoint = nums.length - 1;
+        while (leftPoint < rightPoint) {
+            if (nums[leftPoint] % 2 != 0 && nums[rightPoint] % 2 == 0) {
+                swap(nums, leftPoint, rightPoint);
+                leftPoint++;
+                rightPoint--;
+            }
+            if (nums[leftPoint] % 2 == 0) {
+                leftPoint++;
+            }
+            if (nums[rightPoint] % 2 != 0) {
+                rightPoint--;
+            }
+        }
+        return nums;
+    }
+
+    private void swap(int[] nums, int firstIndex, int secondIndex) {
+        int temp = nums[firstIndex];
+        nums[firstIndex] = nums[secondIndex];
+        nums[secondIndex] = temp;
+    }
+}
